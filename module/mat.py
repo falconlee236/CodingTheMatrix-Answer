0853c72033c43250c3c9752690d3ff064147b1f9
falconlee236 <falconlee236@naver.com>
2021-05-31 11:35:57 +0000

    refactor: modify mat.py by PEP8 style guide

--- module/mat.py
+++ module/mat.py
@@ -190,6 +190,7 @@ def matrix_vector_mul(M, v):
     assert M.D[1] == v.D
     pass
 
+
 def matrix_matrix_mul(A, B):
     """
     Returns the result of the matrix-matrix multiplication, A*B.
@@ -219,7 +220,8 @@ def matrix_matrix_mul(A, B):
     assert A.D[1] == B.D[0]
     pass
 
-################################################################################
+###############################################################################
+
 
 class Mat:
     def __init__(self, labels, function):
@@ -236,14 +238,15 @@ class Mat:
     def __neg__(self):
         return (-1)*self
 
-    def __mul__(self,other):
+    def __mul__(self, other):
         if Mat == type(other):
-            return matrix_matrix_mul(self,other)
+            return matrix_matrix_mul(self, other)
         elif Vec == type(other):
-            return matrix_vector_mul(self,other)
+            return matrix_vector_mul(self, other)
         else:
-            return scalar_mul(self,other)
-            #this will only be used if other is scalar (or not-supported). mat and vec both have __mul__ implemented
+            return scalar_mul(self, other)
+            # this will only be used if other is scalar (or not-supported).
+            # mat and vec both have __mul__ implemented
 
     def __rmul__(self, other):
         if Vec == type(other):
@@ -258,7 +261,7 @@ class Mat:
         if other == 0:
             return self
 
-    def __sub__(a,b):
+    def __sub__(a, b):
         return a+(-b)
 
     __eq__ = equal
@@ -268,16 +271,22 @@ class Mat:
 
     def __str__(M, rows=None, cols=None):
         "string representation for print()"
-        if rows == None: rows = sorted(M.D[0], key=repr)
-        if cols == None: cols = sorted(M.D[1], key=repr)
+        if rows is None:
+            rows = sorted(M.D[0], key=repr)
+        if cols is None:
+            cols = sorted(M.D[1], key=repr)
         separator = ' | '
         numdec = 3
         pre = 1+max([len(str(r)) for r in rows])
-        colw = {col:(1+max([len(str(col))] + [len('{0:.{1}G}'.format(M[row,col],numdec)) if isinstance(M[row,col], int) or isinstance(M[row,col], float) else len(str(M[row,col])) for row in rows])) for col in cols}
-        s1 = ' '*(1+ pre + len(separator))
-        s2 = ''.join(['{0:>{1}}'.format(str(c),colw[c]) for c in cols])
+        colw = {col: (1+max([len(str(col))] + [len('{0:.{1}G}'.format(M[row, col], numdec))
+                                               if isinstance(M[row, col], int) or isinstance(M[row,col], float)
+                                               else len(str(M[row, col])) for row in rows])) for col in cols}
+        s1 = ' '*(1 + pre + len(separator))
+        s2 = ''.join(['{0:>{1}}'.format(str(c), colw[c]) for c in cols])
         s3 = ' '*(pre+len(separator)) + '-'*(sum(list(colw.values())) + 1)
-        s4 = ''.join(['{0:>{1}} {2}'.format(str(r), pre,separator)+''.join(['{0:>{1}.{2}G}'.format(M[r,c],colw[c],numdec) if isinstance(M[r,c], int) or isinstance(M[r,c], float) else '{0:>{1}}'.format(M[r,c], colw[c]) for c in cols])+'\n' for r in rows])
+        s4 = ''.join(['{0:>{1}} {2}'.format(str(r), pre, separator)+''.join(['{0:>{1}.{2}G}'.format(M[r, c], colw[c], numdec)
+                                                                             if isinstance(M[r, c], int) or isinstance(M[r, c], float)
+                                                                             else '{0:>{1}}'.format(M[r, c], colw[c]) for c in cols])+'\n' for r in rows])
         return '\n' + s1 + s2 + '\n' + s3 + '\n' + s4
 
     def pp(self, rows, cols):
@@ -285,7 +294,7 @@ class Mat:
 
     def __repr__(self):
         "evaluatable representation"
-        return "Mat(" + str(self.D) +", " + str(self.f) + ")"
+        return "Mat(" + str(self.D) + ", " + str(self.f) + ")"
 
     def __iter__(self):
         raise TypeError('%r object is not iterable' % self.__class__.__name__)

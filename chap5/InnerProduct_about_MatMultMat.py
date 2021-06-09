bfeac915d1ba015db55fb761bc69ba2b6d365004
falconlee236 <falconlee236@naver.com>
2021-06-09 14:23:19 +0000

    Add: Prepare chap6 Lab

--- chap5/InnerProduct_about_MatMultMat.py
+++ chap5/InnerProduct_about_MatMultMat.py
@@ -16,4 +16,11 @@ voting_data = {x[0]: list(map(int, x[1:len(x) - 1])) for x in data}
 data_set = set(range(len(voting_data['Italy'])))
 voting_data = coldict2mat({key: Vec(data_set, {x: y for x, y in enumerate(value)}) for key, value in voting_data.items()})
 M = voting_data.transpose() * voting_data
-print(M)
+
+result = sorted([(value, key) for key, value in M.f.items()])
+# Question 1
+print(result[0])
+# Question 2
+print(result[:10])
+# Question 3
+print(result[-1])

5bc2de7086dbc52290d624ae7205c40d045f4c26
falconlee236 <falconlee236@naver.com>
2021-06-10 14:12:31 +0000

    Add: Implement Task 6.12.2 and Task 6.12.3

--- chap6/Perspective_Rectification.py
+++ chap6/Perspective_Rectification.py
@@ -10,3 +10,15 @@ def move2board(y):
 
 
 print(move2board(Vec({'y1', 'y2', 'y3'}, {'y1': 3, 'y2': 7, 'y3': 0.2})))
+
+
+# Task 6.12.2
+def make_equations(x1, x2, w1, w2):
+    D = [(a, b) for a in {'y1', 'y2', 'y3'} for b in {'x1', 'x2', 'x3'}]
+    u = [-x1, -x2, -1, 0, 0, 0, w1*x1, w1*x2, w1]
+    v = [0, 0, 0, -x1, -x2, -1, w2*x1, w2*x2, w2]
+    return [Vec(set(D), {D[i]: u[i] for i in range(9)}), Vec(set(D), {D[i]: v[i] for i in range(9)})]
+
+
+# Task 6.12.3
+w = Vec({(a, b) for a in {'y1', 'y2', 'y3'} for b in {'x1', 'x2', 'x3'}}, {('y1', 'x1'): 1})

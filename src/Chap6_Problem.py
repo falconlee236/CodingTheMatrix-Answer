804cbea6eee65eaf72f8b8f1e9651c495e9ef5fe
falconlee236 <falconlee236@naver.com>
2021-06-13 02:14:01 +0000

    Add:Implement Problem 6.14.15

--- src/Chap6_Problem.py
+++ src/Chap6_Problem.py
@@ -39,3 +39,34 @@ print(vec2rep(list(map(list2vec, veclist)), list2vec([6, -4, 27, -3])))
 ''' Testcase 3 '''
 veclist = [[one, 0, one], [one, one, 0], [0, 0, one]]
 print(vec2rep(list(map(list2vec, veclist)), list2vec([0, one, one])))
+
+
+# Problem 6.14.15
+def is_superfluous(L, i):
+    if len(L) == 1:
+        sub_m = coldict2mat(L)
+    else:
+        sub_m = coldict2mat(L[:i] + L[i+1:])
+    residual = L[i] - sub_m * solve(sub_m, L[i])
+    if residual * residual < 1E-14:
+        return True
+    return False
+
+
+''' Testcase 1 '''
+a0 = Vec({'a', 'b', 'c', 'd'}, {'a': 1})
+a1 = Vec({'a', 'b', 'c', 'd'}, {'b': 1})
+a2 = Vec({'a', 'b', 'c', 'd'}, {'c': 1})
+a3 = Vec({'a', 'b', 'c', 'd'}, {'a': 1, 'c': 3})
+print(is_superfluous([a0, a1, a2, a3], 3))
+print(is_superfluous([a0, a1, a2, a3], 0))
+print(is_superfluous([a0, a1, a2, a3], 1))
+''' Testcase 2 '''
+veclist = [[1, 2, 3]]
+print(is_superfluous(list(map(list2vec, veclist)), 0))
+''' Testcase 3 '''
+veclist = [[2, 5, 5, 6], [2, 0, 1, 3], [0, 5, 4, 3]]
+print(is_superfluous(list(map(list2vec, veclist)), 2))
+''' Testcase 4 '''
+veclist = [[one, one, 0, 0], [one, one, one, one], [0, 0, 0, one]]
+print(is_superfluous(list(map(list2vec, veclist)), 2))

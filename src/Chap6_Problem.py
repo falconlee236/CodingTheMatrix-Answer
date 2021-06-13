1a6de922879e2d5c6f7a2186818f8827d4d50f67
falconlee236 <falconlee236@naver.com>
2021-06-13 05:47:10 +0000

    Add: Implement Problem 6.14.17

--- src/Chap6_Problem.py
+++ src/Chap6_Problem.py
@@ -10,6 +10,7 @@ def rep2vec(u, veclist):
     return coldict2mat(veclist) * u
 
 
+print("\nProblem 6.14.13")
 ''' Testcase 1 '''
 a0 = Vec({'a', 'b', 'c', 'd'}, {'a': 1})
 a1 = Vec({'a', 'b', 'c', 'd'}, {'b': 1})
@@ -28,6 +29,7 @@ def vec2rep(veclist, v):
     return solve(coldict2mat(veclist), v)
 
 
+print("\nProblem 6.14.14")
 ''' Testcase 1 '''
 a0 = Vec({'a', 'b', 'c', 'd'}, {'a': 1})
 a1 = Vec({'a', 'b', 'c', 'd'}, {'b': 1})
@@ -53,6 +55,7 @@ def is_superfluous(L, i):
     return False
 
 
+print("\nProblem 6.14.15")
 ''' Testcase 1 '''
 a0 = Vec({'a', 'b', 'c', 'd'}, {'a': 1})
 a1 = Vec({'a', 'b', 'c', 'd'}, {'b': 1})
@@ -70,3 +73,32 @@ print(is_superfluous(list(map(list2vec, veclist)), 2))
 ''' Testcase 4 '''
 veclist = [[one, one, 0, 0], [one, one, one, one], [0, 0, 0, one]]
 print(is_superfluous(list(map(list2vec, veclist)), 2))
+
+
+# Problem 6.14.16
+def is_independent(L):
+    for i in range(len(L)):
+        if is_superfluous(L, i) is True:
+            return False
+    return True
+
+
+print("\nProblem 6.14.16")
+''' Testcase 1 '''
+a0 = Vec({'a', 'b', 'c', 'd'}, {'a': 1})
+a1 = Vec({'a', 'b', 'c', 'd'}, {'b': 1})
+a2 = Vec({'a', 'b', 'c', 'd'}, {'c': 1})
+a3 = Vec({'a', 'b', 'c', 'd'}, {'a': 1, 'c': 3})
+print(is_independent([a0, a1, a2]))
+print(is_independent([a0, a2, a3]))
+print(is_independent([a0, a1, a3]))
+print(is_independent([a0, a1, a2, a3]))
+''' Testcase 2 '''
+veclist = [[2, 4, 0], [8, 16, 4], [0, 0, 7]]
+print(is_independent(list(map(list2vec, veclist))))
+''' Testcase 3 '''
+veclist = [[1, 3, 0, 0], [2, 1, 1, 0], [1, 1, 4, -1]]
+print(is_independent(list(map(list2vec, veclist))))
+''' Testcase 4 '''
+veclist = [[one, 0, one, 0], [0, one, 0, 0], [one, one, one, one], [one, 0, 0, one]]
+print(is_independent(list(map(list2vec, veclist))))

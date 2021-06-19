ca8953e846be4f300c546a9e708955edf88201a0
falconlee236 <falconlee236@naver.com>
2021-06-19 14:38:12 +0000

    Add: Solve Task 8.8.7

--- src/Chap8_Lab_Factorization.py
+++ src/Chap8_Lab_Factorization.py
@@ -1,8 +1,9 @@
-from factoring_support import intsqrt, gcd, dumb_factor
+from factoring_support import intsqrt, gcd, dumb_factor, primes
 from GF2 import one
 from vec import Vec
 
 
+print("Task 8.8.1")
 # Task 8.8.1
 def root_method(N):
     for x in range(1, N):
@@ -15,6 +16,7 @@ def root_method(N):
 print(list(map(root_method, [55, 77, 146771, 118])))
 
 
+print("Task 8.8.2")
 # Task 8.8.2
 r = 65165321354
 s = 187465732135446
@@ -25,6 +27,7 @@ d = gcd(a, b)
 print(a % d == 0 and b % d == 0 and d >= s)
 
 
+print("Task 8.8.3")
 # Task 8.8.3
 N = 367160330145890434494322103
 a = 67469780066325164
@@ -35,6 +38,7 @@ print(N % d == 0)
 print(d)
 
 
+print("Task 8.8.4")
 # Task 8.8.4
 primeset = {2, 3, 5, 7, 11, 13}
 print(dumb_factor(12, primeset))
@@ -44,6 +48,7 @@ print(dumb_factor(2 * 17, primeset))
 print(dumb_factor(2 * 3 * 5 * 7 * 19, primeset))
 
 
+print("Task 8.8.5")
 # Task 8.8.5
 def int2GF2(i):
     return one if i % 2 == 1 else 0
@@ -53,6 +58,7 @@ print(int2GF2(3))
 print(int2GF2(4))
 
 
+print("Task 8.8.6")
 # Task 8.8.6
 def make_Vec(primeset, factors):
     return Vec(primeset, {a: int2GF2(b) for (a, b) in factors})
@@ -62,6 +68,26 @@ print(make_Vec({2, 3, 5, 7, 11}, [(3, 1)]))
 print(make_Vec({2, 3, 5, 7, 11}, [(2, 17), (3, 0), (5, 1), (11, 3)]))
 
 
+print("Task 8.8.7")
+# Task 8.8.7
+def find_candidates(N, primeset):
+    roots = []
+    rowlist = []
+    i = 2
+    while len(roots) <= len(primeset) and len(rowlist) <= len(primeset):
+        x = intsqrt(N) + i
+        fact_vec = dumb_factor(x * x - N, primeset)
+        if fact_vec:
+            roots.append(x)
+            rowlist.append(fact_vec)
+        i += 1
+    return roots, [make_Vec(primeset, row) for row in rowlist]
+
+
+roots, rowlist = find_candidates(2419, primes(32))
+print(roots)
+for x in rowlist:
+    print(x)
 
 
 

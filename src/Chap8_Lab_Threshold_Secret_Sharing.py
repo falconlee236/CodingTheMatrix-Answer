99e18dc228f67343c9ed2d6e31d86d71fb026e65
falconlee236 <falconlee236@naver.com>
2021-06-19 09:09:39 +0000

    Add: Complete Task 8.7.2

--- src/Chap8_Lab_Threshold_Secret_Sharing.py
+++ src/Chap8_Lab_Threshold_Secret_Sharing.py
@@ -1,4 +1,5 @@
 from vecutil import list2vec
+from independence import is_independent
 from GF2 import one
 from random import randint
 
@@ -21,4 +22,15 @@ def choose_secret_vector(s, t):
 
 # Task 8.7.2
 while True:
-    
+    flag = False
+    vec_list = [(a0, b0)] + [(list2vec([randGF2() for x in range(6)]), list2vec([randGF2() for y in range(6)])) for i in range(3)]
+    for i in range(2):
+        for j in range(i + 1, 3):
+            for k in range(j + 1, 4):
+                res_list = [vec_list[i][0], vec_list[i][1], vec_list[j][0], vec_list[j][1], vec_list[k][0], vec_list[k][1]]
+                if is_independent(res_list) is False:
+                    flag = True
+    if flag is False:
+        res = {1: (res_list[0], res_list[1]), 2: (res_list[2], res_list[3]), 3: (res_list[4], res_list[5])}
+        break
+print(res)

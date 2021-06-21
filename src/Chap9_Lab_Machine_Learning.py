0a86545a8c599a74879030151c254f26085cf578
falconlee236 <falconlee236@naver.com>
2021-06-21 10:08:19 +0000

    Add: Solve Task 9.4.2

--- src/Chap9_Lab_Machine_Learning.py
+++ src/Chap9_Lab_Machine_Learning.py
@@ -1,13 +1,17 @@
 from cancer_data import read_training_data
+from vec import Vec
 
 
 # Task 9.4.1
 A, b = read_training_data("train.data")
 
 
+# Task 9.4.2
+def signum(u):
+    return Vec(u.D, {key: 1 if u[key] >= 0 else -1 for key in u.D})
 
 
-
+print(signum(Vec({'A', 'B'}, {'A': 3, 'B': -2})))
 
 
 

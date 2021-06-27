f8ed2f96789082ae43148aa5322356478bab9127
falconlee236 <falconlee236@naver.com>
2021-06-27 07:17:49 +0000

    Add: Solve Problem 10.11.12

--- src/Chap10_Problem.py
+++ src/Chap10_Problem.py
@@ -1,6 +1,10 @@
 from vecutil import list2vec
-from matutil import coldict2mat
+from matutil import coldict2mat, mat2rowdict
 from orthonormalization import orthonormalize, aug_orthonormalize
+from triangular import triangular_solve
+from mat import Mat
+from vec import Vec
+import QR
 
 
 print("# Problem 10.11.9")
@@ -22,13 +26,26 @@ print(coldict2mat(Qlist) * coldict2mat(Rlist))
 print(coldict2mat(Qlist) * coldict2mat(Rlist) - coldict2mat(L))
 
 
+print("# Problem 10.11.12")
+# Problem 10.11.12
 
 
+def QR_solve(A, b):
+    Q, R = QR.factor(A)
+    return triangular_solve(mat2rowdict(R), sorted(A.D[1], key=repr), Q.transpose() * b)
 
 
+A = Mat(({'a', 'b', 'c'}, {'A', 'B'}), {('a', 'A'): -1, ('a', 'B'): 2, ('b', 'A'): 5, ('b', 'B'): 3, ('c', 'A'): 1, ('c', 'B'): -2})
+print(A)
 
+Q, R = QR.factor(A)
+print(Q)
+print(R)
 
-
+b = Vec({'a', 'b', 'c'}, {'a': 1, 'b': -1})
+x = QR_solve(A, b)
+print(x)
+print(A.transpose() * (b - A*x))
 
 
 
---
+++ src/dictutil.py
@@ -0,0 +1,5 @@
+# Copyright 2013 Philip N. Klein
+def dict2list(dct, keylist): return [dct[i] for i in keylist]
+
+
+def list2dict(L, keylist): return {key: value for key, value in zip(keylist, L)}

008d0a04d8ba72fd51ee8be5658e04945595b434
falconlee236 <falconlee236@naver.com>
2021-06-28 14:03:37 +0000

    Add: Solve Task 11.9.2

--- src/Chap11_Lab_Wavelets.py
+++ src/Chap11_Lab_Wavelets.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 # Task 11.9.1
 def forward_no_normalization(v):
     D = {}
@@ -15,11 +18,16 @@ v = [1, 2, 3, 4]
 print(forward_no_normalization(v))
 v = [4, 5, 3, 7, 4, 5, 2, 3, 9, 7, 3, 5, 0, 0, 0, 0]
 print(forward_no_normalization(v))
+print()
 
 
+# Task 11.9.2
+def normalize_coefficients(n, D):
+    return {key: value*sqrt(n/(4*key[0])) if key != (0, 0) else value * sqrt(n) for key, value in D.items()}
 
 
-
+print(normalize_coefficients(4, {(2, 0): 1, (2, 1): 1, (1, 0): 1, (0, 0): 1}))
+print(normalize_coefficients(4, forward_no_normalization([1, 2, 3, 4])))
 
 
 

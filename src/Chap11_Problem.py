07d476718edfc9e5bd48a327353513c2e5c12d8f
falconlee236 <falconlee236@naver.com>
2021-06-30 14:16:34 +0000

    Add: Solve  Problem 11.11.2

--- src/Chap11_Problem.py
+++ src/Chap11_Problem.py
@@ -8,7 +8,7 @@ print("# Problem 11.11.1")
 
 
 def orthogonal_vec2rep(Q, b):
-    return b * Q.transpose()
+    return Q * b
 
 
 Q = Mat((set(range(3)), set(range(3))), {(0, 0): 1/sqrt(2), (0, 1): 1/sqrt(2),
@@ -16,6 +16,20 @@ Q = Mat((set(range(3)), set(range(3))), {(0, 0): 1/sqrt(2), (0, 1): 1/sqrt(2),
                                         (2, 0): -1/sqrt(6), (2, 1): 1/sqrt(6), (2, 2): 2/sqrt(6)})
 b = Vec({0, 1, 2}, {0: 10, 1: 20, 2: 30})
 print(orthogonal_vec2rep(Q, b))
+
+
+print("\n# Problem 11.11.2")
+# Problem 11.11.2
+
+
+def orthogonal_change_of_basis(A, B, a):
+    return a * A * B
+
+
+A = Q
+B = Q
+a = Vec({0, 1, 2}, {0: sqrt(2), 1: 1/sqrt(3), 2: 2/sqrt(6)})
+print(orthogonal_change_of_basis(A, B, a))
 
 
 

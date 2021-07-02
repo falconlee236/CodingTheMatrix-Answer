ec79b6a5f5e36ef709fc6dddf40ac9def52aff9e
falconlee236 <falconlee236@naver.com>
2021-07-02 14:46:07 +0000

    Add: ReSolve Task 12.6.1

--- src/Chap12_Lab_Eigenface.py
+++ src/Chap12_Lab_Eigenface.py
@@ -1,9 +1,17 @@
 from eigenfaces import load_images
+from image import image2display
 from vec import Vec
 
 
 # Task 12.6.1
 faces_img = load_images("../img/faces")
+'''
 D = [(x, y) for x in range(166) for y in range(189)]
+faces_vec = {key: for key, value in faces_img.items()}
 faces_vec = {key: Vec(set(D), {x: value[x[1]][x[0]] for x in D}) for key, value in faces_img.items()}
-print(len(faces_vec))
+'''
+
+
+# Task 12.6.2
+print(faces_img[0][0])
+# image2display(faces_img[0])

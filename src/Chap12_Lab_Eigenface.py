4d1958081f2b2b3fdfc5cf510ca9b0547f2f1c0f
falconlee236 <falconlee236@naver.com>
2021-07-03 01:43:31 +0000

    Add: Solve Task 12.6.3

--- src/Chap12_Lab_Eigenface.py
+++ src/Chap12_Lab_Eigenface.py
@@ -1,5 +1,8 @@
 from eigenfaces import load_images
 from image import image2display
+from matutil import rowdict2mat, mat2coldict, coldict2mat
+from svd import factor
+from vec import Vec
 import copy
 
 
@@ -28,3 +31,49 @@ def make_centeringVec(imgdict):
 
 
 center_img = make_centeringVec(faces_img)
+
+
+# Task 12.6.3
+D = [(x, y) for x in range(166) for y in range(189)]
+center_vec = {key: Vec(set(D), {x: value[x[1]][x[0]] for x in D}) for key, value in center_img.items()}
+A = rowdict2mat(center_vec)
+u, w, v = factor(A)
+sub_m = mat2coldict(v)
+M = coldict2mat({i: sub_m[i] for i in range(10)})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

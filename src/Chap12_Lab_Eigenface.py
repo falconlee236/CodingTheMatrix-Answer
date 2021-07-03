fca1843a2f19bfcbdc950068194b603ae66f0b31
falconlee236 <falconlee236@naver.com>
2021-07-03 06:05:36 +0000

    Add: Solve Task 12.6.12 AND Finish Lab : Eigenface

--- src/Chap12_Lab_Eigenface.py
+++ src/Chap12_Lab_Eigenface.py
@@ -11,7 +11,7 @@ faces_img = load_images("../img/faces")
 
 
 # Task 12.6.2
-def make_centeringVec(imgdict):
+def make_centerVec(imgdict):
     center = []
     vec_len = len(imgdict)
     for i in range(189):
@@ -22,6 +22,10 @@ def make_centeringVec(imgdict):
                 res += img[i][j]
             res_list.append(res/vec_len)
         center.append(res_list)
+    return center
+
+
+def make_centeringVec(imgdict, center):
     centering_img = copy.deepcopy(imgdict)
     for img in centering_img.keys():
         for i in range(189):
@@ -30,7 +34,8 @@ def make_centeringVec(imgdict):
     return centering_img
 
 
-center_img = make_centeringVec(faces_img)
+center = make_centerVec(faces_img)
+center_img = make_centeringVec(faces_img, center)
 
 
 # Task 12.6.3
@@ -80,15 +85,16 @@ for x in distance_list:
 
 # Task 12.6.8
 unclass_img = load_images("../img/unclassified", n=11)
-center_unclass_img = make_centeringVec(unclass_img)
+center_unclass = make_centerVec(unclass_img)
+center_unclass_img = make_centeringVec(unclass_img, center_unclass)
 D = [(x, y) for x in range(166) for y in range(189)]
-center_vec = {key: Vec(set(D), {x: value[x[1]][x[0]] for x in D}) for key, value in center_unclass_img.items()}
-A = rowdict2mat(center_vec)
-distance_list = {x: distance_squared(y, M) for x, y in mat2rowdict(A).items()}
+center_unclass_vec = {key: Vec(set(D), {x: value[x[1]][x[0]] for x in D}) for key, value in center_unclass_img.items()}
+B = rowdict2mat(center_unclass_vec)
+distance_unclass_list = {x: distance_squared(y, M) for x, y in mat2rowdict(B).items()}
 print("Task 12.6.8")
-for x in distance_list:
+for x in distance_unclass_list:
     print(f"num = {x}  | ", end="")
-    print(format(distance_list[x], ','))
+    print(format(distance_unclass_list[x], ','))
 
 
 # Task 12.6.10
@@ -96,9 +102,26 @@ def project(x, M):
     return M * projected_representation(x, M)
 
 
+# Task 12.6.11
+def face_project(D, center):
+    project_list = {x: project(y, M) for x, y in mat2rowdict(D).items()}
+    original_project = {key: [[value[(i, j)] for i in range(166)] for j in range(189)] for key, value in project_list.items()}
+    for img in original_project.keys():
+            for i in range(189):
+                for j in range(166):
+                    original_project[img][i][j] += center[i][j]
+    return original_project
 
 
+original_project = face_project(A, center)
+for x in range(len(original_project)):
+    image2display(original_project[x])
 
+
+# Task 12.6.12
+original_project = face_project(B, center_unclass)
+for x in range(len(original_project)):
+    image2display(original_project[x])
 
 
 

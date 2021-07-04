0a1f09195f36a04fd3fd3e95dfa8aeb14627aeac
falconlee236 <falconlee236@naver.com>
2021-07-04 14:09:24 +0000

    Add: Solve Problem 12.8.7

--- src/Chap12_Problem.py
+++ src/Chap12_Problem.py
@@ -1,5 +1,7 @@
 from mat import Mat
+from vecutil import list2vec
 from matutil import listlist2mat
+from svd import factor
 
 
 # Problem 12.8.1
@@ -8,3 +10,41 @@ def squared_Frob(A):
 
 
 print(squared_Frob(listlist2mat([[1, 2, 3, 4], [-4, 2, -1, 0]])))
+
+
+# Problem 12.8.7
+def SVD_solve(U, Sigma, V, b):
+    try:
+        answer = V * Mat(Sigma.D, {key: 1/value for key, value in Sigma.f.items()}) * U.transpose() * b
+    except ZeroDivisionError:
+        return "FAIL"
+    else:
+        return answer
+
+
+A = listlist2mat([[1, 1, 0], [1, 0, 1], [0, 1, 1]])
+U, Sigma, V = factor(A)
+b = list2vec([2, 3, 3])
+print(SVD_solve(U, Sigma, V, b))
+
+A = listlist2mat([[1, 1, 1], [1, 1, 1], [0, 1, 1]])
+U, Sigma, V = factor(A)
+b = list2vec([2, 3, 3])
+print(SVD_solve(U, Sigma, V, b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

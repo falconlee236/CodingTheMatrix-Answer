833201a68886d718e1f6b8c843979579f9d7d732
falconlee236 <falconlee236@naver.com>
2021-07-05 14:37:36 +0000

    Add: Solve Task 13.12.2

--- src/Chap13_Lab_PageRank.py
+++ src/Chap13_Lab_PageRank.py
@@ -13,13 +13,19 @@ def find_num_links(L):
 print(find_num_links(small_links))
 
 
+# Task 13.12.2
+print(" # Task 13.12.2")
 
 
+def make_Markov(L):
+    num_links = find_num_links(L)
+    for i in L.f:
+        L[i] /= num_links[i[1]]
 
 
-
-
-
+print(small_links)
+make_Markov(small_links)
+print(small_links)
 
 
 

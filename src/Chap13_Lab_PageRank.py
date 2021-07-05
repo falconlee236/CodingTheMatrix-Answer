2440edb1e914cdd21eb9f3c17ed3b8667f1ede75
falconlee236 <falconlee236@naver.com>
2021-07-05 14:45:23 +0000

    Add: Task 13.12.3 Temp file

--- src/Chap13_Lab_PageRank.py
+++ src/Chap13_Lab_PageRank.py
@@ -1,5 +1,6 @@
 from pagerank_test import small_links, A2
 from vec import Vec
+from math import sqrt
 
 
 # Task 13.12.1
@@ -28,9 +29,20 @@ make_Markov(small_links)
 print(small_links)
 
 
+# Task 13.12.3
+print(" # Task 13.12.3")
 
 
+def power_method(A1, k):
+    v = Vec(A1.D[1], {key: 1 for key in A1.D[1]})
+    for i in range(k):
+        u = A1*v
+        print(sqrt((v * v) / (u * u)))
+        v = u
+    return v
 
+
+print(power_method(A2, 100))
 
 
 

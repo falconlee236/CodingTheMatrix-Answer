c423eff95a4e38c8e3e568e8a78d5efa56262750
falconlee236 <falconlee236@naver.com>
2021-07-05 14:22:49 +0000

    Add: Solve Task 13.12.1

--- src/Chap13_Lab_PageRank.py
+++ src/Chap13_Lab_PageRank.py
@@ -2,11 +2,15 @@ from pagerank_test import small_links, A2
 from vec import Vec
 
 
+# Task 13.12.1
+print("# Task 13.12.1")
 
 
+def find_num_links(L):
+    return Vec(L.D[0], {key: 1 for key in L.D[0]}) * L
 
 
-
+print(find_num_links(small_links))
 
 
 

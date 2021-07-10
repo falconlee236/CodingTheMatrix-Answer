1275a80f22ffc1dad58854018ecf8c8f8ad3faba
falconlee236 <falconlee236@naver.com>
2021-07-10 09:24:03 +0000

    Add: Finish This Boook!!!!!!!! Congratulation! Good Bye CodingtheMatrix

--- src/Chap14_Problem.py
+++ src/Chap14_Problem.py
@@ -12,7 +12,6 @@ def find_move_helper(A, r):
 
 
 A = Mat(({1, 2, 3}, {1, 2, 3}), {(1, 1): 1, (1, 2): 1, (2, 2): 1, (2, 3): 1, (3, 1): 1, (3, 3): 1})
-print(find_move_helper(A, 3))
 
 
 print("# Problem 14.16.3")
@@ -24,12 +23,23 @@ def find_move_direction(A, x, r):
 
 
 x = Vec({1, 2, 3}, {1: 2, 2: 4, 3: 6})
-print(find_move_direction(A, x, 3))
 
 
+print("# Problem 14.16.4")
+# Problem 14.16.4
 
 
+def find_move(A, x, r):
+    w = find_move_direction(A, x, r)
+    sigma = 0
+    for i in range(100):
+        sigma = i
+        test = list((x + sigma * w).f.values())
+        if min(test) >= 0 and (min(w.f.values()) > 0 or len(list(filter(lambda x: x < 10e-10, test))) > 0):
+            return sigma
 
+
+print(find_move(A, x, 3))
 
 
 

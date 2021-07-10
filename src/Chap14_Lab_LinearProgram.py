673c36babcf7cf9ecb9bed8f0f15f289a36a2812
falconlee236 <falconlee236@naver.com>
2021-07-10 01:26:50 +0000

    Add: Solve Task 14.13.2

--- src/Chap14_Lab_LinearProgram.py
+++ src/Chap14_Lab_LinearProgram.py
@@ -5,27 +5,36 @@ from vec import Vec
 
 features = {'area(worst)', 'smoothness(worst)', 'texture(mean)'}
 A, b = read_training_data("../src/data/train.data", features)
+print("Task 14.13.1")
 # Task 14.13.1
 
 
 def main_constraint(i, a_i, d_i, features):
     if d_i > 0:
-        return Vec(features | {i, "gamma"}, dict(a_i.f.items() | {i: 1, "gamma": -1}.items()))
+        return Vec(features | {"gamma"} | A.D[0], dict(a_i.f.items() | {i: 1, "gamma": -1}.items()))
     else:
-        a_i *= -1
-        return Vec(features | {i, "gamma"}, dict(a_i.f.items() | {i: 1, "gamma": 1}.items()))
+        return Vec(features | {"gamma"} | A.D[0], dict((-1 * a_i).f.items() | {i: 1, "gamma": 1}.items()))
 
 
 # 861799
 a_i = mat2rowdict(A)[861799]
 b_i = b[861799]
-print(b_i)
 print(main_constraint(861799, a_i, b_i, features))
 
 
+print("Task 14.13.2")
+# Task 14.13.2
 
 
+def make_matrix(feature_vectors, diagnoses, features):
+    res = {}
+    for key, value in feature_vectors.items():
+        res[key] = main_constraint(key, value, diagnoses[key], features)
+        res[-key] = Vec(res[key].D, {key: 1})
+    return rowdict2mat(res)
 
+
+A = make_matrix(mat2rowdict(A), b, features)
 
 
 

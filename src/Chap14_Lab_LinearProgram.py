c91c8a4a790e6bfc6b443b82c102abacfbb392be
falconlee236 <falconlee236@naver.com>
2021-07-10 05:14:05 +0000

    Add: Finish Chap14_ Lab: linear program

--- src/Chap14_Lab_LinearProgram.py
+++ src/Chap14_Lab_LinearProgram.py
@@ -2,6 +2,7 @@ from cancer_data import read_training_data
 from matutil import rowdict2mat, mat2rowdict
 from vec import Vec
 import simplex
+import random
 
 
 features = {'area(worst)', 'smoothness(worst)', 'texture(mean)'}
@@ -53,11 +54,33 @@ print("Task 14.13.5")
 # Task 14.13.5
 b = make_b(A)
 c = make_c(A, features)
+rand_list = list(A.D[0])
+random.shuffle(rand_list)
+sublist = []
+for i in range(len(A.D[1])):
+    sublist.append(rand_list[i])
+    sublist.append(-rand_list[i])
+    if(len(set(sublist)) == len(A.D[1])):
+        break
+R_square = set(sublist)
+simplex.find_vertex(A, b, R_square)
+x_hat = simplex.optimize(A, b, c, R_square)
 
 
+print("Task 14.13.6")
+# Task 14.13.6
 
 
+def C(feature_vector, x_hat):
+    gamma = x_hat['gamma']
+    w = Vec(features, {key: x_hat[key] for key in features})
+    return 1 if w * feature_vector > gamma else -1
 
+
+# Task 14.13.7
+A_origin, b_origin = read_training_data("../src/data/train.data", features)
+error_vec = Vec(b_origin.D, {key: C(value, x_hat) for key, value in mat2rowdict(A_origin)})
+print(error_vec * b_origin)
 
 
 

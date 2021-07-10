aaf07cc0a8f31072a5fcff854c882966ed2f9b35
falconlee236 <falconlee236@naver.com>
2021-07-10 05:57:18 +0000

    Add: Solve Problem 14.16.3

--- src/Chap14_Problem.py
+++ src/Chap14_Problem.py
@@ -3,6 +3,7 @@ from vec import Vec
 from solver import solve
 
 
+print("# Probem 14.16.2")
 # Probem 14.16.2
 
 
@@ -12,3 +13,30 @@ def find_move_helper(A, r):
 
 A = Mat(({1, 2, 3}, {1, 2, 3}), {(1, 1): 1, (1, 2): 1, (2, 2): 1, (2, 3): 1, (3, 1): 1, (3, 3): 1})
 print(find_move_helper(A, 3))
+
+
+print("# Problem 14.16.3")
+# Problem 14.16.3
+
+
+def find_move_direction(A, x, r):
+    return find_move_helper(A, r)
+
+
+x = Vec({1, 2, 3}, {1: 2, 2: 4, 3: 6})
+print(find_move_direction(A, x, 3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
